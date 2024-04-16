04c5bab2a1740b32d1ad94615523926ff31b85b7
Taminogg <gutjahrt190063@sus.htl-grieskirchen.at>
2024-04-16 11:47:33 +0200

    routes updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { FunctionOverviewPageComponent } from './function-overview-page/function-overview-page.component';
-import { ContainerRequestPageComponent } from './conainter-request/container-request-page/container-request-page.component';
-import { EditOrAddCsContainerRequestOrderPageComponent } from './conainter-request/edit-or-add-order-page-cr-cs/edit-or-add-cs-container-request-order-page.component';
 import { ChatForOrderComponent } from './chat-for-order/chat-for-order.component';
-import { EditTlContainerRequestOrderPageComponent } from './conainter-request/edit-order-page-cr-tl/edit-tl-container-request-order-page.component';
-import { EditOrAddCsProductionPlanningOrderPageComponent } from './conainter-request/edit-or-add-order-page-pp-cs/edit-or-add-cs-production-planning-order-page.component';
 import { AdminComponent } from './admin/admin.component';
-import { EditPPProductionPlanningOrderPageComponent } from './conainter-request/edit-order-page-pp-pp/edit-pp-production-planning-order-page.component';
 import { SettingsComponent } from './settings/settings.component';
+import { EditPPProductionPlanningOrderPageComponent } from './request/edit-order-page-pp-pp/edit-pp-production-planning-order-page.component';
+import { EditTlContainerRequestOrderPageComponent } from './request/edit-order-page-cr-tl/edit-tl-container-request-order-page.component';
+import { EditOrAddCsProductionPlanningOrderPageComponent } from './request/edit-or-add-order-page-pp-cs/edit-or-add-cs-production-planning-order-page.component';
+import { ContainerRequestPageComponent } from './request/request-overview-page/request-overview-page.component';
+import { EditOrAddCsContainerRequestOrderPageComponent } from './request/edit-or-add-order-page-cr-cs/edit-or-add-cs-container-request-order-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/function-overview-age', pathMatch: 'full' },
